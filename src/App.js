6f58727911ae7566d7e4200832ce1151d147c227
Luyanda Madonsela <luyandasmadonsela@gmail.com>
2022-10-20 19:20:02 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
+
 import Navigation from "./navigation.js"
 import HomePageBody from "./homePageBody.js"
 import Footer from  "./footer.js"
 import {Route, Routes}from "react-router-dom"
 
 function App() {
-    return (
-            <>
-                <Navigation/>
-                <HomePageBody/>
-                <Footer/>
-            </>
-        
+    return ( 
+        <Routes>
+            <Route path="/" element={
+                <div className="page">
+                    <Navigation/>
+                    <HomePageBody/>
+                    <Footer/>
+                </div>
+            }/>
+
+            <Route path="/certificates" element={
+                <div className="page">
+                    <Navigation/>
+                    <h1>certificates</h1>
+                    <Footer/>
+                </div>
+            }/>
+
+            <Route path="/projects" element={
+                <div className="page">
+                    <Navigation/>
+                    <h1>projects</h1>
+                    <Footer/>
+                </div>
+            }/>
+
+            <Route path="/contact" element={
+                <div className="page">
+                    <Navigation/>
+                    <h1>contact</h1>
+                    <Footer/>
+                </div>
+            }/>
+
+            <Route path="/career" element={
+                <div className="page">
+                    <Navigation/>
+                    <h1>career</h1>
+                    <Footer/>
+                </div>
+            }/>
+        </Routes>
     )
 }
 
